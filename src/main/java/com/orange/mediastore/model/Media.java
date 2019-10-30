fcc5e8b7b9324de19319ea148982440fffdd29ec
Mihai.Poenaru <mihai.poenaru@orange.com>
2019-10-30 12:30:31 +0200

    added image uri

--- src/main/java/com/orange/mediastore/model/Media.java
+++ src/main/java/com/orange/mediastore/model/Media.java
@@ -3,6 +3,7 @@ package com.orange.mediastore.model;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -13,6 +14,7 @@ public abstract class Media {
     @Indexed(unique = true)
     public String title;
     public LocalDate release;
+    public URI imageUri;
     public String description;
     public String director;
     public Set<String> actors;
